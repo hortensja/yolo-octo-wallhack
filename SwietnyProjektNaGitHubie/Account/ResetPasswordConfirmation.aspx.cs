b3eea00e4d103abbee0cf729fba5b187a48cad1b
hortensja <hortensja.p@gmail.com>
2015-06-28 21:21:14 +0200

    jhgfkj

---
+++ SwietnyProjektNaGitHubie/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SwietnyProjektNaGitHubie.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SwietnyProjektNaGitHubie/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SwietnyProjektNaGitHubie.Startup))]
+namespace SwietnyProjektNaGitHubie
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
